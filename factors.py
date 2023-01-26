58a04e175a0ad475937e9be46cee8bcd70e7ee18
GideonAmhaG <gideonamaha@gmail.com>
2023-01-26 14:46:13 +0000

    Misc

---
+++ factors.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" RSA factorization: simply factorize """
+import sys
+
+
+with open(sys.argv[1], "r", encoding="utf-8") as f:
+    for line in f:
+        n = int(line)
+        p = 2
+        q = 0
+        while p * 2 <= n:
+            if n % p == 0:
+                q = n // p
+                print("{:d}={:d}*{:d}".format(n, q, p))
+                break
+            else:
+                p += 1
+
